ab2f5954e629bb2e4550eb2254fc23f1e77e1361
Jimeno0 <c.p.jimeno0@gmail.com>
2019-10-19 13:33:31 +0200

    initial commit 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import View from './App'
-import { ThemeProvider } from './context'
 
 const App = (props) => {
   return (
-    <ThemeProvider>
-      <View />
-    </ThemeProvider>
+    <View />
   )
 }
 
